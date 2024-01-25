9e0d9d994d749000b78be1d81c4bf5f65a33a673
mventura14 <m.ventura14@gmail.com>
2024-01-25 17:36:00 -0600

    Set up archieve page

--- js/archive.js
+++ js/archive.js
@@ -2,12 +2,20 @@
    const closeButton = document.querySelector("[data-close-modal]")
    const modal = document.querySelector("[data-modal]")
 
-   openButton.addEventListener("click", ()=>{
-    modal.showModal();
-   })
+   
 
    closeButton.addEventListener("click", ()=>{
     modal.close();
    })
 
    modal.addEventListener("click",(e) => {closeDialog(e)} )
+
+
+   const projectCardArr = document.querySelectorAll(".card-cont .proj-card")
+
+   projectCardArr.forEach( card =>{
+      card.addEventListener("click", ()=>{
+         console.log("clicked")
+         modal.showModal()
+      })
+   })
