9fe25f9c087f4de301ad43e8648b558d6cb0e2f7
mventura14 <m.ventura14@gmail.com>
2024-02-02 12:01:05 -0600

    added first componenet into archieve

--- js/archive.js
+++ js/archive.js
@@ -1,21 +1,38 @@
-   const openButton = document.querySelector("[data-open-modal]")
-   const closeButton = document.querySelector("[data-close-modal]")
-   const modal = document.querySelector("[data-modal]")
-
-   
-
-   closeButton.addEventListener("click", ()=>{
-    modal.close();
-   })
-
-   modal.addEventListener("click",(e) => {closeDialog(e)} )
 
 
-   const projectCardArr = document.querySelectorAll(".card-cont .proj-card")
+   (() => {
+"use strict"
 
-   projectCardArr.forEach( card =>{
-      card.addEventListener("click", ()=>{
-         console.log("clicked")
-         modal.showModal()
+      const openButton = document.querySelector("[data-open-modal]")
+      const closeButton = document.querySelector("[data-close-modal]")
+      const modal = document.querySelector("[data-modal]")
+      const previewFrame = modal.querySelector('#previewModal')
+      const allCards = document.querySelectorAll('.proj-card')
+
+
+      closeButton.addEventListener("click", () => {
+         modal.close();
       })
-   })
+
+      modal.addEventListener("click", (e) => { closeDialog(e) })
+
+
+      const projectCardArr = document.querySelectorAll(".card-cont .proj-card")
+
+      projectCardArr.forEach(card => {
+         card.addEventListener("click", () => {
+            console.log("clicked")
+            modal.showModal()
+         })
+      })
+
+      allCards[0].addEventListener('click', ()=>{
+         previewFrame.src = 'archive/components/comp-01/component02.html'
+      })
+      
+
+
+   })();
+
+
+
